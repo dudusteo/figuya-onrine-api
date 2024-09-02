17a3a66bbb1941bc8db14af27a6252d598a44b20
DuDuSteo <p.aleksiejuk@protonmail.com>
2024-09-02 22:18:01 +0200

    Production test

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -1,3 +1,3 @@
 Rails.application.config.active_storage.resolve_model_to_route = :rails_storage_proxy
 Rails.application.config.active_storage.variant_processor = :vips
-Rails.application.routes.default_url_options[:host] = ENV['HOST']
+Rails.application.routes.default_url_options[:host] = ENV.fetch('HOST')
